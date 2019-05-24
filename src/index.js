31aef0ac2ae75c4e5bcbf96911dcb1a8eeafcafb
Alexey Nikitin <nikitin010@gmail.com>
2019-05-24 11:11:39 +0300

    Scroll lock

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
 
-document.querySelector('body').onscroll = function(e) {
+document.querySelector("body").addEventListener(
+  "touchmove",
+  function(e) {
     e.preventDefault();
-};
+  },
+  { passive: false }
+);
